8013265f32657f71576c4114ed4461295d973e54
Uğur Arabacı <33137597+ugurarabaci@users.noreply.github.com>
2022-06-14 11:46:11 +0300

    directory deleted due to major changes

--- ws/src/main/java/com/ugurcrud/ws/repository/FlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ugurcrud.ws.repository;
-
-import com.ugurcrud.ws.entity.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlightRepository extends JpaRepository<Flight, Integer> {
-}
--- ws/src/main/java/com/ugurcrud/ws/repository/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ugurcrud.ws.repository;
-
-import com.ugurcrud.ws.entity.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Integer> {
-}
